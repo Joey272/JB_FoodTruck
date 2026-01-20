12130a8b5a58ef315d337a5fd690e7a98a5078bd
Joey272 <beckey.joseph.27@c-dh.org>
2026-01-20 13:40:18 -0600

    .

---
+++ City.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class City {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String customerOrder = scan.nextLine();
+        FoodTruck(customerOrder);
+    }
+}
---
+++ FoodTruck.java
@@ -0,0 +1,8 @@
+public class FoodTruck {
+    public FoodTruck(String order){
+        System.out.println("here is your food"+order);
+    }
+    public static void main(String[] args) {
+        
+    }
+}
