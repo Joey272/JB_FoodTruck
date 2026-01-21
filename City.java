3235f017589954282a127d6a43e7a2ea0cca0e9b
Joey272 <beckey.joseph.27@c-dh.org>
2026-01-21 14:41:22 -0600

    .

--- City.java
+++ City.java
@@ -1,9 +1,8 @@
-import java.util.Scanner;
 
 public class City {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String customerOrder = scan.nextLine();
-        FoodTruck(customerOrder);
+        
+        FoodTruck FoodT = new FoodTruck();
+        System.out.println(FoodT);
     }
 }
--- FoodTruck.java
+++ FoodTruck.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class FoodTruck {
-    public FoodTruck(String order){
-        System.out.println("here is your food"+order);
+    public FoodTruck(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("what do you want to order");
+        String order = scan.nextLine();
+        System.out.println("here is your food "+"("+order+")");
+        scan.close();
     }
     public static void main(String[] args) {
         
